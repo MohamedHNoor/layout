9a99d2906ff7374e3bad1b6b49fb52459050ce67
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-06-12 22:39:53 +0200

    Update links.js

--- src/utils/links.js
+++ src/utils/links.js
@@ -2,7 +2,7 @@ const links = [
   { id: 1, text: 'Home', path: '/' },
   { id: 2, text: 'About', path: 'about' },
   { id: 3, text: 'Blog', path: 'blog' },
-  { id: 4, text: 'Reserve a Car', path: 'reserve' },
+  { id: 4, text: 'Book', path: 'book' },
   { id: 5, text: 'Reservations', path: 'my-reservations' },
 ];
 
